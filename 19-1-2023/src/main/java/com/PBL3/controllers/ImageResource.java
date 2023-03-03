5efac5457e9cf6b06010ed065f8eda26805b0b65
Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-03-03 16:26:32 +0700

    Handle Image save && get

---
+++ 19-1-2023/src/main/java/com/PBL3/controllers/ImageResource.java
@@ -0,0 +1,52 @@
+package com.PBL3.controllers;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.GZIPOutputStream;
+
+@WebServlet("/img/*")
+public class ImageResource extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String UPLOAD_DIRECTORY = "public";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        OutputStream out = resp.getOutputStream();
+        String encodings = req.getHeader("Accept-Encoding");
+		resp.setHeader("Content-Encoding", "x-compress");
+        String pathInfo = req.getPathInfo();
+        String[] parts = pathInfo.substring(1).split("/");
+
+        String uploadPath = "D:\\PBL3.1" + File.separator + UPLOAD_DIRECTORY + File.separator + parts[0];
+        Path path = Paths.get(uploadPath);
+        String filetype = Files.probeContentType(path);
+        if (filetype == null) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        resp.setContentType(filetype);
+        File file = new File(uploadPath);
+        FileInputStream in = new FileInputStream(file);
+        IOUtils.copy(in, out);
+        out.close();
+        in.close();
+
+    }
+
+
+}
---
+++ 19-1-2023/src/main/java/com/PBL3/utils/helpers/HandleImage.java
@@ -0,0 +1,87 @@
+package com.PBL3.utils.helpers;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandleImage {
+    private static final String UPLOAD_DIRECTORY = "public";
+
+    // upload settings
+    private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3; // 3MB
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; // 50MB
+
+    public static String save(HttpServletRequest request){
+
+        ArrayList<String> imagepath = new ArrayList<String>();
+
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            System.out.println(("Error: Form must has enctype=multipart/form-data."));
+            return null;
+        }
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // sets memory threshold - beyond which files are stored in disk
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // sets temporary location to store files
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        // sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+
+        // sets maximum size of request (include file + form data)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+
+        String uploadPath = "D:\\PBL3.1" + File.separator + UPLOAD_DIRECTORY;
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+        try {
+            List<FileItem> formItems = upload.parseRequest(request);
+
+            if (formItems != null && formItems.size() > 0) {
+                // iterates over form's fields
+                        String id = IDGeneration.generate();
+                for (FileItem item : formItems) {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+
+
+                        String[] fileNameSplits = fileName.split("\\.");
+                        int extensionIndex = fileNameSplits.length - 1;
+                        String newfilename = id + "." + fileNameSplits[extensionIndex];
+                        imagepath.add(newfilename);
+
+                        String filePath = uploadPath + File.separator + newfilename;
+                        File storeFile = new File(filePath);
+
+                        // saves the file on disk
+                        item.write(storeFile);
+                        System.out.println("Successfully");
+                        return "\\" + newfilename;
+                    }
+                }
+            }
+        } catch (Exception ex) {
+//			request.setAttribute("message", "There was an error: " + ex.getMessage());
+            System.out.println("Error to save image");
+            return null;
+        }
+            return null;
+    }
+//    public static FileInputStream getImgae(String filename){
+//        return new FileInputStream(filename);
+//    }
+}
