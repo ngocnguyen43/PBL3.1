5dad7482b293051e4e42b4d8f9f964be2fb4ca5c
Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-05-04 07:41:20 +0700

    #35 - get all companies

--- 19-1-2023/src/main/java/com/PBL3/controllers/admin/company/CompanyController.java
+++ 19-1-2023/src/main/java/com/PBL3/controllers/admin/company/CompanyController.java
@@ -1,5 +1,9 @@
 package com.PBL3.controllers.admin.company;
 
+import com.PBL3.services.ICompanyService;
+import com.PBL3.utils.exceptions.ErrorHandler;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -12,10 +16,17 @@ import static com.PBL3.utils.Constants.EndPoint.PRIVATE;
 import static com.PBL3.utils.Constants.EndPoint.V1;
 
 @MultipartConfig
-@WebServlet(urlPatterns = {V1 + PRIVATE + "/company"})
+@WebServlet(urlPatterns = {V1 + PRIVATE + "/companies"})
 public class CompanyController extends HttpServlet {
+    @Inject
+    private ICompanyService companyService;
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ErrorHandler.handle(resp,()->companyService.getAllcompanies());
     }
 }
---
+++ 19-1-2023/src/main/java/com/PBL3/services/ICompanyService.java
@@ -0,0 +1,7 @@
+package com.PBL3.services;
+
+import com.PBL3.utils.response.Message;
+
+public interface ICompanyService {
+    Message getAllcompanies();
+}
