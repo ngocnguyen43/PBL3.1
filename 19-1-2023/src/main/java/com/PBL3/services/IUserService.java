be14b7a5886c1a1f38c84ed4472a2d93eed59f76
Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-05-08 21:43:37 +0700

    #40 - update user bug

--- 19-1-2023/src/main/java/com/PBL3/services/IUserService.java
+++ 19-1-2023/src/main/java/com/PBL3/services/IUserService.java
@@ -2,10 +2,7 @@ package com.PBL3.services;
 
 import com.PBL3.dtos.UserDTO;
 import com.PBL3.dtos.pagination.UserPaginationDTO;
-import com.PBL3.utils.exceptions.dbExceptions.CreateFailedException;
-import com.PBL3.utils.exceptions.dbExceptions.DuplicateEntryException;
-import com.PBL3.utils.exceptions.dbExceptions.InvalidPropertiesException;
-import com.PBL3.utils.exceptions.dbExceptions.UpdateFailedException;
+import com.PBL3.utils.exceptions.dbExceptions.*;
 import com.PBL3.utils.response.Message;
 
 public interface IUserService {
@@ -21,5 +18,5 @@ public interface IUserService {
 
     Message findOne(String id);
 
-    Message update(UserDTO dto, String id) throws DuplicateEntryException, UpdateFailedException;
+    Message update(UserDTO dto, String id) throws DuplicateEntryException, UpdateFailedException, UnexpectedException, NotFoundException;
 }
