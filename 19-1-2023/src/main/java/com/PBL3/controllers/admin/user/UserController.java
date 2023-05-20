30927da46e00442b1e1a2b9e2b2e743e370f7f83
Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-05-20 10:03:36 +0700

    #57 - add notifcation when creating new user and updating user by admin

--- 19-1-2023/src/main/java/com/PBL3/controllers/admin/user/UserController.java
+++ 19-1-2023/src/main/java/com/PBL3/controllers/admin/user/UserController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static com.PBL3.utils.Constants.EndPoint.PRIVATE;
 import static com.PBL3.utils.Constants.EndPoint.V1;
 
 @WebServlet(urlPatterns = {V1 + "/user/*"})
@@ -40,12 +39,12 @@ public class UserController extends HttpServlet {
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getPathInfo().substring(1);
         UserDTO dto = Helper.paramsToString(req.getParameterMap()).toModel(UserDTO.class);
-        ErrorHandler.handle(resp, () -> userService.update(dto, id));
+        ErrorHandler.handle(resp, () -> userService.update(dto, id, req.getHeader("client_id")));
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getPathInfo().substring(1);
-        ErrorHandler.handle(resp, () -> userService.delete(id));
+        ErrorHandler.handle(resp, () -> userService.delete(id, req.getHeader("client_id")));
     }
 }
