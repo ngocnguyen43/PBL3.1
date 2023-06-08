ff8a7ce78af0e0e6d4bdb448e50859c8afe01737
Nguyen Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-06-08 09:01:09 +0700

    clean

--- 19-1-2023/src/main/java/com/PBL3/utils/helpers/CheckContainsFile.java
+++ 19-1-2023/src/main/java/com/PBL3/utils/helpers/CheckContainsFile.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class CheckContainsFile {
     static public boolean check(HttpServletRequest req) {
         try {
-            return req.getPart("file") != null && req.getPart("file").getSize() > 0;
+            return req.getPart("image") != null && req.getPart("image").getSize() > 0;
         } catch (IOException | ServletException e) {
             return false;
         }
